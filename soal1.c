5b76a9582fb9e53324baad4e75d4c53fdde175db
nm-mutia <nm.mutia02@gmail.com>
2017-11-16 17:27:27 +0700

    cblg

--- soal1.c
+++ soal1.c
@@ -10,12 +10,11 @@
 
 static const char *dirpath = "/home/mutia/Documents";
 
-
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
   int res;
   char fpath[1000];
-  char newFile[100];
+  /*char newFile[100];
   printf("path   : %s, len: %ld\n", path, strlen(path));
   if (strcmp(path, "/") != 0) {
     memcpy(newFile, path, strlen(path) - 4);
@@ -24,7 +23,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
     memcpy(newFile, path, strlen(path));
   }
   printf("newFile: %s\n", newFile);
-  sprintf(fpath,"%s%s",dirpath, newFile);
+  sprintf(fpath,"%s%s",dirpath, newFile);*/
   res = lstat(fpath, stbuf);
 
   if (res == -1)
@@ -36,13 +35,13 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
            off_t offset, struct fuse_file_info *fi)
 {
- char fpath[1000];
-  if(strcmp(path,"/") == 0)
+  char fpath[1000];
+  /*if(strcmp(path,"/") == 0)
   {
     path=dirpath;
     sprintf(fpath,"%s",path);
   }
-  else sprintf(fpath, "%s%s",dirpath,path);
+  else sprintf(fpath, "%s%s",dirpath,path);*/
   int res = 0;
 
   DIR *dp;
@@ -56,13 +55,13 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     return -errno;
 
   while ((de = readdir(dp)) != NULL) {
-    char *newName;
-    newName = strcat(de->d_name, ".ditandai");
+    //char *newName;
+    //newName = strcat(de->d_name, ".ditandai");
     struct stat st;
     memset(&st, 0, sizeof(st));
     st.st_ino = de->d_ino;
     st.st_mode = de->d_type << 12;
-    res = (filler(buf, newName, &st, 0));
+    res = (filler(buf, de->d_name, &st, 0));
       if(res!=0) break;
   }
 
@@ -71,49 +70,66 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 }
 
 
-/*static int xmp_open(const char *path, struct fuse_file_info *fi)
+static int xmp_open(const char *path, struct fuse_file_info *fi)
 {
-	int res, len; 
-//num=0, flag=0;
+	int res;
+	//int len; 
 	char fpath[1000];
 	sprintf(fpath, "%s%s", dirpath,path);
-	strcpy(path, fpath);
 	
+	/*strcpy(access, fpath);	
 	len = strlen(path);
-	if((len >= 2) && strcmp(&(path[len - 4]), ".pdf") == 0){
+	if((len >= 2) && strcmp(&(access[len - 4]), ".pdf") == 0){
 		char command[100];
 		sprintf(command,"zenity --error --text='Terjadi kesalahan! File berisi konten berbahaya.'");
 		system(command);
 	}
-	else if((len >= 2) && strcmp(&(path[len - 4]), ".doc") == 0){
+	else if((len >= 2) && strcmp(&(access[len - 4]), ".doc") == 0){
 		char command[100];
 		sprintf(command,"zenity --error --text='Terjadi kesalahan! File berisi konten berbahaya.'");
 		system(command);
 	}
-	else if((len >= 2) && strcmp(&(path[len - 4]), ".txt") == 0){
+	else if((len >= 2) && strcmp(&(access[len - 4]), ".txt") == 0){
 		char command[100];
 		sprintf(command,"zenity --error --text='Terjadi kesalahan! File berisi konten berbahaya.'");
 		system(command);
-	}
+	}*/	
 
-	res = open(fpath, fi->flags); //langsung dibuka file
+	char ch, sf[1000], tf[1000];
+	FILE *from, *to;
+	sprintf(sf, "%s", fpath);
+	from = fopen(sf, "rb");
+	sprintf(tf,"%s.ditandai",fpath);
+
+	int ava;
+	ava = access(tf, F_OK);
+	if(ava == 0) unlink(tf);
+	to = fopen(tf, "wb");
+	
+	while((ch = fgetc(from))!=EOF) fputc(ch, to);
+	char command[100];
+	sprintf(command,"chmod 000 '%s.ditandai'", fpath);
+	system(command);
+	
+	fclose(from);
+	fclose(to);
+
+	res = open(fpath, fi->flags); 
 	if(res == -1)
 		return -errno;
 
-
 	close(res);
 	return 0;
-}*/
+}
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
         struct fuse_file_info *fi)
 {
   char fpath[1000];
-  char newFile[100];
+  /*char newFile[100];
   if(strcmp(path,"/") == 0)
   {
     memcpy(newFile, path, strlen(path));
-//    path=dirpath;
     sprintf(fpath,"%s",newFile);
   }
   else {
@@ -121,7 +137,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
     newFile[strlen(path) - 9] = '\0';
 
     sprintf(fpath, "%s%s",dirpath,newFile);
-  }
+  }*/
   int res = 0;
   int fd = 0 ;
 
@@ -140,7 +156,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 static struct fuse_operations xmp_oper = {
   .getattr  = xmp_getattr,
   .readdir  = xmp_readdir,
-  //.open	    = xmp_open,
+  .open	    = xmp_open,
   .read   = xmp_read,
 };
 
