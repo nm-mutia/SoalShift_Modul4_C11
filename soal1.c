2517d98fb8b7217234f883d01d0060851bb21546
nm-mutia <nm.mutia02@gmail.com>
2017-11-16 16:08:38 +0700

    nyoba lagi

--- soal1.c
+++ soal1.c
@@ -13,44 +13,48 @@ static const char *dirpath = "/home/mutia/Documents";
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
   int res;
-  char fpath[1000];
-  sprintf(fpath,"%s%s",dirpath,path);
-  res = lstat(fpath, stbuf);
-  if (res == -1)
-    return -errno;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
 
-  return 0;
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
-
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-           off_t offset, struct fuse_file_info *fi)
+		       off_t offset, struct fuse_file_info *fi)
 {
- char fpath[1000];
-  int res = 0;
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
 
-  DIR *dp;
-  struct dirent *de;
+	DIR *dp;
+	struct dirent *de;
 
-  (void) offset;
-  (void) fi;
+	(void) offset;
+	(void) fi;
 
-  dp = opendir(fpath);
-  if (dp == NULL)
-    return -errno;
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
 
-  while ((de = readdir(dp)) != NULL) {
-    char *newName;
-    newName = strcat(de->d_name, "");
-    struct stat st;
-    memset(&st, 0, sizeof(st));
-    st.st_ino = de->d_ino;
-    st.st_mode = de->d_type << 12;
-    res = (filler(buf, newName, &st, 0));
-      if(res!=0) break;
-  }
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
 
-  closedir(dp);
-  return 0;
+	closedir(dp);
+	return 0;
 }
 
 static int xmp_open(const char *path, struct fuse_file_info *fi)
@@ -61,7 +65,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 	st.st_atime = time(NULL) = lastaccess;
 	sprintf(fpath, "%s%s", dirpath,path);
 	//strcpy(lastaccess,fpath);
-	strcy(st.st_atime, fpath);
+	strcpy(st.st_atime, fpath);
 	
 	len = strlen(lastaccess);
 	if((len >= 2) && strcmp(&(lastaccess[len - 4]), ".pdf") == 0){
@@ -90,7 +94,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 }
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-        struct fuse_file_info *fi)
+		    struct fuse_file_info *fi)
 {
   char fpath[1000];
 	if(strcmp(path,"/") == 0)
@@ -99,26 +103,26 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		sprintf(fpath,"%s",path);
 	}
 	else sprintf(fpath, "%s%s",dirpath,path);
-  int res = 0;
+	int res = 0;
   int fd = 0 ;
 
-  (void) fi;
-  fd = open(fpath, O_RDONLY);
-  if (fd == -1)
-    return -errno;
+	(void) fi;
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
 
-  res = pread(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
 
-  close(fd);
-  return res;
+	close(fd);
+	return res;
 }
 
 static struct fuse_operations xmp_oper = {
   .getattr  = xmp_getattr,
   .readdir  = xmp_readdir,
-  //.open	    = xmp_open,
+  .open	    = xmp_open,
   .read   = xmp_read,
 };
 
