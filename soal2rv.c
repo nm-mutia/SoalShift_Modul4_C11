e6fa540d82a3142527478357af8a76ef3a68347b
nm-mutia <nm.mutia02@gmail.com>
2017-11-20 16:50:44 +0700

    revisi soal2 +1

--- soal2rv.c
+++ soal2rv.c
@@ -59,6 +59,29 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
+static int xmp_mkdir(const char *path, mode_t mode)
+{
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+
+static int xmp_rename(const char *from, const char *to)
+{
+	int res;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
 const char *ext(const char *filename) {
     const char *dot = strrchr(filename, '.');
     if(!dot || dot == filename) return "";
@@ -77,16 +100,24 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	else sprintf(fpath, "%s%s",dirpath,path);
 	int res = 0;
   	int fd = 0 ;
-	
+	char hasil[1000];	
+
 	if(strcmp(ext(path),"pdf")==0 || strcmp(ext(path),"doc")==0 || strcmp(ext(path),"txt")==0){
-		char read[1000];
+		char read[1000], dirmake[1000];
 		sprintf(read, "%s.ditandai", fpath);
-		system("chmod 000 /home/mutia/Documents/*.ditandai");
 		system("zenity --error --text='Terjadi kesalahan! File berisi konten berbahaya.'");
 		rename(fpath, read);
-	
-		system("mkdir /home/mutia/Documents/rahasia");
-		system("mv /home/mutia/Documents/*.ditandai /home/mutia/Documents/rahasia");	
+		strcpy(dirmake, dirpath); 					
+		strcat(dirmake, "/rahasia");
+		
+		DIR *rahasia;
+		rahasia = opendir(dirmake);
+		if(rahasia == NULL){
+			mkdir(dirmake, 0777);		
+		}
+		
+		sprintf(hasil, "mv %s %s",read,dirmake);
+		system(hasil);
 		system("chmod 000 /home/mutia/Documents/rahasia/*.ditandai");
 		return -errno;
 	}
@@ -107,6 +138,8 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 static struct fuse_operations xmp_oper = {
 	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
+	.rename 	= xmp_rename,
+	.mkdir		= xmp_mkdir,
 	.read		= xmp_read,
 };
 
